193edbe98bf41b06a03b2c0636593e3878cbcbdc
류지현 <dudvndwlgus@naver.com>
2022-09-24 15:44:25 +0900

    초기 커밋.

---
+++ C++/C++3주차.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include<array>
+#include<string>
+
+using namespace std;
+
+/* 1. 숫자의 자릿수 출력하기
+int countdigits(int num) {
+	int a = 0;
+	while (num > 0) {
+		num = num / 10;
+		a++;
+	}
+	return a;
+}
+int countdigits1(string num1) {
+
+	int a = stoi(num1);
+
+	return countdigits(a);
+
+}
+
+int main(void) {
+
+
+	int num = 12345;
+	string num1 = "12345";
+
+	cout << "숫자 12345 의 자릿수는 ? ";
+	cout << countdigits(num)<<endl;
+	cout << "문자열 \"12345\"의 자릿수는 ? ";
+	cout << countdigits1(num1);
+
+
+
+
+	return 0;
+
+}*/
+
