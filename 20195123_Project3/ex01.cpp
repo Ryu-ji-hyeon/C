2a85ea5a98c8624ac3a1184c3acc43deb0ef1f1b
류지현 <dudvndwlgus@naver.com>
2022-11-12 18:41:18 +0900

    과제 3

---
+++ 20195123_Project3/ex01.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+
+using namespace std;
+
+class Vector {
+private:
+	float x, y, z;
+public:
+	Vector() {};
+	Vector(float x, float y, float z) :x{ x }, y{ y }, z{ z }{}
+
+	void print() {
+		cout << x << " " << y << " " << z << endl;
+	}
+	//1. 단항연산자 +
+    Vector operator+()const
+	{
+		return Vector(+x, +y, +z);
+	}
+	//2. 단항연산자 -
+	Vector operator-()const
+	{
+		return Vector(-x, -y, -z);
+	}
+	//3. 이항연산자 +
+	Vector operator+(const Vector& v)const
+	{
+		return Vector(x + v.x, y + v.y, z + v.z);
+	}
+	//4. 이항연산자 -
+	 Vector operator-(const Vector& v)const
+	{
+		return Vector(x - v.x, y - v.y, z - v.z);
+	}
+	//5. 이항연산자 * (내적 계산)
+	float operator*(const Vector& v)const
+	{
+		float dot = (x * v.x) + (y * v.y) + (z* v.z);
+		return dot;
+	}
+	//5. 이항연산자 * (상수 곱하기)
+	Vector operator*(float n)const
+	{
+		return Vector{ x * n,y * n,z * n };
+	}
+	//6. 이항연산자 /
+	Vector operator/(float n)const
+	{
+		return Vector{ x / n,y / n,z / n };
+	}
+	//7. 전위 연산자 ++
+	Vector operator++()
+	{
+		
+		++x;
+		++y;
+		++z;
+		return *this;
+	}
+	//7. 전위 연산자 --
+	Vector operator--()
+	{
+
+		--x;
+		--y;
+		--z;
+		return *this;
+	}
+	//8. 후위 연산자 ++
+	Vector operator++(int)
+	{
+		Vector tmp = *this;
+		++x;
+		++y;
+		++z;
+		return tmp;
+	}
+	//8. 후위 연산자 --
+	Vector operator--(int)
+	{
+		Vector tmp = *this;
+		--x;
+		--y;
+		--z;
+		return tmp;
+	}
+};
+int main() {
+
+	Vector v0{ 0,1,2 };
+	Vector v1{ 1,2,3 };
+
+	Vector v2 = v0 + v1;//v0.operator+(v1);
+	v2.print();
+
+	Vector v3 = -v1;
+	v3.print();
+
+	float v4 = v0 * v1;
+	cout << v4 << endl;
+
+	Vector v5 = v1 * 3.0f;
+	v5.print();
+
+	Vector v6 = ++v1;
+	v6.print();
+	Vector v7 = v1++;
+	v7.print();
+	v1.print();
+
+
+}
--- C++/C++3주차.cpp
+++ C++/C++3주차.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include<array>
 #include<string>
+#include<vector>
+#include<cstdlib>
+#include<ctime>
+
 
 using namespace std;
 
-/* 1. 숫자의 자릿수 출력하기
-int countdigits(int num) {
+ // 1. 숫자의 자릿수 출력하기
+/*int countdigits(int num) {
 	int a = 0;
 	while (num > 0) {
 		num = num / 10;
@@ -38,4 +42,128 @@ int main(void) {
 	return 0;
 
 }*/
+//2. 트리 순회하기 (실습문제 트리로 구현 x)
+
+/*typedef struct node* nodePointer;
+typedef struct node {
+	int data;
+	nodePointer left, right;
+}node;
+
+//전위
+void preorder(nodePointer pointer) {
+	if (pointer) {
+		cout << pointer->data << " ";
+		preorder(pointer->left);
+		preorder(pointer->right);
+	}
+}
+//중위
+void inorder(nodePointer pointer) {
+	if (pointer) {
+		inorder(pointer->left);
+		cout << pointer->data << " ";
+		inorder(pointer->right);
+	}
+}
+//후위
+void postorder(nodePointer pointer) {
+	if (pointer) {
+		postorder(pointer->left);
+		postorder(pointer->right);
+		cout << pointer->data << " ";
+	
+	}
+}
+int main(void) {
+	node nodes[10];
+
+	for (int i = 0; i <= 9; i++) {
+		nodes[i].data = i;
+		nodes[i].left = NULL;
+		nodes[i].right = NULL;
+	}
+	for (int i = 1; i <= 9; i++) {
+		if (i % 2 == 0) {
+			nodes[i / 2].left = &nodes[i];
+		}
+		else {
+			nodes[i / 2].right = &nodes[i];
+		}
+	}
+	preorder(&nodes[0]);
+	cout << endl;
+	inorder(&nodes[0]);
+	cout << endl;
+	postorder(&nodes[0]);
+	cout << endl;
+
+	return 0;
+		
+}*/
+
+// 3.2차원 벡터 z방향으로 탐색하기
+int main() {
+
+	
+	vector<vector<int>> vec;
+	srand((unsigned int)time(NULL));
+	
+
+	for (int i = 0; i < 4; i++) {
+		vector<int> row;
+		
+		for (int j = 0; j < 4; j++) {
+			row.push_back(rand() % 6);
+		}
+		vec.push_back(row);
+	}
+	cout << "2차원 벡터 출력" << endl;
+	for (int i = 0; i < 4; i++) {
+
+		for (int j = 0; j < 4; j++) {
+			cout << vec[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << "2차원 벡터 z방향 탐색과정" << endl;
+
+
+	for (int i = 0; i < 4; i++) {
+		
+		for (int j = 0; j < 4; j++) {
+			if (i % 2 == 0 && j % 2 == 0) {
+				if(i==0)
+				     cout<<vec[0 ][ 0]<<" ";
+				else
+					cout << vec[i][j + 1] << " ";
+			}
+			if (i % 2 == 0 && j % 2 == 1) {
+				if(i==0)
+					cout<< vec[0][1] << " ";		
+				else
+					cout << vec[i - 1][j - 1] << " ";
+			}
+			if (i % 2 == 1 && j % 2 == 0) {
+				cout << vec[i][ j + 1]<<" ";
+			}
+			if (i % 2 == 1 && j % 2 == 1) {
+				cout << vec[i-1][j + 1]<<" ";
+				if(i==1&&j==3)
+					cout<<vec[i + 1][ j -3]<<" ";
+				if (i == 3 && j == 3)
+					break;
+			}
+		}
+		cout << endl;
+	}
+}
+/*int& z(const vector<vector<int>> vec) {
+
+}*/
+
+
+
+
+
 
